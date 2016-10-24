8711eb9f949f0e691fb0c6b0f5a6cd897046b040
JD-RAMOS\Juan <jd.ramosf@gmail.com>
2016-10-23 23:44:02 -0500

    Modified file

--- taskone.py
+++ taskone.py
@@ -5,11 +5,8 @@ import json
 # Create the dictionary with the values for token and github
 data = {"token": "4cf3ff970884c0176fc273c844a53db8", "github": "https://github.com/jdramosf/taskone.git"}
 
-# Convert dictionary to JSON format
-json_data = json.dumps(data)
-
-# Make the HTTP POST request to the URL using the data in the json_data variable
-r = requests.post("http://challenge.code2040.org/api/register", json_data)
+# Make the HTTP POST request to the URL using the data in the data variable
+r = requests.post("http://challenge.code2040.org/api/register", params = data)
 
 # Print the response
 print(r)
